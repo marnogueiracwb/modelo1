fd119d30ea45931e6417060fa89d3080bc2960ee
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.0.181>
2017-03-02 19:49:35 -0300

    teste

---
+++ src/Modelo/Inicio.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class Inicio
+{
+
+    public static void main(String[] args)
+    {
+
+        // TODO Auto-generated method stub
+        System.out.println("ola...");
+        System.out.close();
+    }
+
+}
